bf8fc066e09d3b6e30bbc96ce096927b1492ee8b
wangsihuo <1347504493@qq.com>
2018-09-06 11:46:04 +0800

    tijiao demo 9/6

--- src/main/webapp/busi-js/pnsResult.js
+++ src/main/webapp/busi-js/pnsResult.js
@@ -1,11 +1,37 @@
 $(function() {
 
-    var date =new Date().getTime();
+    $.datetimepicker.setLocale('zh');
+    $("#date_yy-mm-dd").datetimepicker({
+        format: 'Y-m-d',
+        minView:'month',
+        onShow:function( ct ){
+            this.setOptions({
+                maxDate: new Date(),
+            });
+        },
 
+    });
+
+
+    var nowdate = new Date().Format("yyyy-MM-dd");
+    $("#date_yy-mm-dd").val(nowdate);
+    var dateFirst =new Date().getTime();
+    getPnsData(dateFirst);
+
+    var sel1=document.getElementById("date_yy-mm-dd");
+    sel1.onchange=function(){
+        $("#resultPns").empty();
+        var date =new Date($("#date_yy-mm-dd").val()).getTime();
+        getPnsData(date);
+    }
+
+});
+
+
+function getPnsData(date) {
     req = {
         "date" : date
     }
-
     $.ajax({
         type: 'POST',
         url: fillPath("readPnsData.do"),
@@ -23,23 +49,39 @@ $(function() {
             $("#resultPns").append($tr);
             for(item in data){
                 var $tr = $("<tr>");
-                 $tr.append('<td style="width:50px;">'+data[item].notify_id+'</td>');
-                 $tr.append('<td style="width:50px;">'+data[item].notify_time+'</td>');
-                 $tr.append('<td style="width:50px;">'+data[item].notify_type+'</td>');
-                 $tr.append('<td style="width:50px;">'+data[item].sign_type+'</td>');
-                 $tr.append('<td style="width:50px;">'+data[item].version+'</td>');
-                 $tr.append('<td style="width:50px;">'+data[item]._input_charset+'</td>');
-                 delete data[item]["notify_id"];
-                 delete data[item]["notify_time"];
-                 delete data[item]["notify_type"];
-                 delete data[item]["sign_type"];
-                 delete data[item]["version"];
-                 delete data[item]["_input_charset"];
-                 delete data[item]["sign"];
-                 $tr.append('<td width="30%">'+JSON.stringify(data[item])+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item].notify_id+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item].notify_time+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item].notify_type+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item].sign_type+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item].version+'</td>');
+                $tr.append('<td style="width:50px;">'+data[item]._input_charset+'</td>');
+                delete data[item]["notify_id"];
+                delete data[item]["notify_time"];
+                delete data[item]["notify_type"];
+                delete data[item]["sign_type"];
+                delete data[item]["version"];
+                delete data[item]["_input_charset"];
+                delete data[item]["sign"];
+                $tr.append('<td width="30%">'+JSON.stringify(data[item])+'</td>');
                 $("#resultPns").append($tr);
             }
         },
 
     });
-});
+}
+
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "H+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
